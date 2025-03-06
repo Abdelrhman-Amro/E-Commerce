124cb27f1040ef6e9fcf90f7a064b22671c813b9
Abdelrhman-Amro <abdamro288dev@gmail.com>
2025-03-06 23:03:06 +0200

    feat(users): :triangular_flag_on_post: add logout view

--- E_Commerce_API/users/views.py
+++ E_Commerce_API/users/views.py
@@ -1,7 +1,9 @@
 from django.db import transaction
-from rest_framework import permissions, serializers, viewsets
+from rest_framework import permissions, serializers, status, viewsets
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import CustomUser, Seller
 from .permissions import IsMarketOwner, IsSeller
@@ -34,6 +36,26 @@ class UserRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class LogoutView(CreateAPIView):
+    """
+    Logout a user by blacklisting their refresh token
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(
+                {"detail": "Successfully logged out."}, status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class SellerViewSet(viewsets.ModelViewSet):
