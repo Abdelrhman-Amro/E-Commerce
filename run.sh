21f7c26a927990ab0aed9f4aaf545a72860e5238
Abdelrhman-Amro <abdamro288dev@gmail.com>
2025-03-13 22:45:17 +0200

    fix deployment #1

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 
 set -e
 cd E_Commerce_API/
-# gunicorn E_Commerce_API.wsgi --log-file -
-gunicorn E_Commerce_API.wsgi:application --bind 0.0.0.0:8000 --workers 4 --log-file -
+gunicorn E_Commerce_API.wsgi --log-file -
